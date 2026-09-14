67921dcc9d8197e488d406037adce0f124fe43bc
alanacarolinasantos48-droid <alanacarolinasantos48@gmail.com>
2026-09-14 12:01:10 -0300

    Ativar modo de check-in por QR Code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import QrCheckin from "./QrCheckin.jsx";
 import "./index.css";
+
+const params = new URLSearchParams(window.location.search);
+const isQrCheckin = params.get("checkin") === "1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isQrCheckin ? <QrCheckin /> : <App />}
   </React.StrictMode>
 );
